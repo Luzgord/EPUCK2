7f16220d4d5d37ec322335cbdd1186a3e7dc2bbb
Emixm4 <maxime.luyet@epfl.ch>
2024-05-09 21:38:54 +0200

    Propre à relire, petit warning à corriger, régler KD

--- src/audio_processing.c
+++ src/audio_processing.c
@@ -135,7 +135,7 @@ void processAudioData(int16_t *data, uint16_t num_samples)
     }
 }
 
-// /**
+ // /**
 //  * @brief Returns a pointer to a specified audio buffer.
 //  *
 //  * @param name Name of the buffer to get.
