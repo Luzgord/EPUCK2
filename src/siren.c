3c2228d30801a3bb6d07564e771b222bbf165f76
Emixm4 <maxime.luyet@epfl.ch>
2024-05-09 13:21:02 +0200

    tout finctionnent, plus qu'à mettre au propre

--- src/siren.c
+++ src/siren.c
@@ -1,7 +1,7 @@
-#include <.git>
+// #include <ch.h>
 #include <hal.h>
-#include <usbcfg.h>
-#include <chprintf.h>
+// #include <usbcfg.h>
+// #include <chprintf.h>
 #include <leds.h>
 
 #include "siren.h"
@@ -13,19 +13,27 @@ static THD_FUNCTION(ThdSiren, arg) {
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
 
-	set_rgb_led(LED2, 0, 0, RGB_MAX_INTENSITY); //blue LEDS first
-	set_rgb_led(LED6,  0, 0, RGB_MAX_INTENSITY);
+
+	bool first_blink = true;
 
     while(1) {
 		//sirens always active
 		//toggling the LEDS in an X pattern
 		if(get_enabled_giro()){
-			toggle_rgb_led(LED2, BLUE_LED, RGB_MAX_INTENSITY);
-			toggle_rgb_led(LED4, RED_LED, RGB_MAX_INTENSITY);
-			toggle_rgb_led(LED6, BLUE_LED, RGB_MAX_INTENSITY);
-			toggle_rgb_led(LED8, RED_LED, RGB_MAX_INTENSITY);
+			if(first_blink){
+				set_rgb_led(LED2, 0, 0, RGB_MAX_INTENSITY); //blue LEDS first
+				set_rgb_led(LED6,  0, 0, RGB_MAX_INTENSITY);
+				first_blink = false;
+			} else {
+				toggle_rgb_led(LED2, BLUE_LED, RGB_MAX_INTENSITY);
+				toggle_rgb_led(LED4, RED_LED, RGB_MAX_INTENSITY);
+				toggle_rgb_led(LED6, BLUE_LED, RGB_MAX_INTENSITY);
+				toggle_rgb_led(LED8, RED_LED, RGB_MAX_INTENSITY);
+			}
+			
 		} else {
 			clear_leds();
+			first_blink = true;
 		}
 
 		chThdSleepMilliseconds(500);
